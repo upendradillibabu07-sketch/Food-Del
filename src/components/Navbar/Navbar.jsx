2045edd0b1ea0125aa5e1b4575eb9eab30461add
upendra_5943 <upendradillibabu07@gmail.com>
2025-12-30 18:10:16 +0530

    navjs-changed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
     <div className='navbar'>
         <img src={assets.logo} alt="" className='logo'/>
         <ul className="navbar-menu">
-            <li onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>santhosh</li>
+            <li onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>home</li>
             <li onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>menu</li>
             <li onClick={()=>setMenu("mobile-app")} className={menu==="mobile-app"?"active":""}>mobile-app</li>
             <li onClick={()=>setMenu("contact us")} className={menu==="contact us"?"active":""}>contact us</li>
