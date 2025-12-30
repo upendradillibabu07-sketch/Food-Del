588b280785b2bc22c407be43ae5aebe24901ac2f
upendra_5943 <upendradillibabu07@gmail.com>
2025-12-30 16:57:26 +0530

    fist-commit

---
+++ src/components/ExploreMenu/ExploreMenu.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './ExploreMenu.css'
+import { menu_list } from '../../assets/assets'
+
+const ExploreMenu = () => {
+  return (
+    <div className='explore-menu' id='explore-menu'>
+        <h1>Explore our menu</h1>
+        <p className='explore-menu-text'>Choose from a diverse menu featuring a delecable array of dishes crafted with the finest ingredients and culinary expertise. Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time.</p>
+        <div className="explore-menu-list">
+            {menu_list.map((item,index)=>{
+                return(
+                 <div key={index} classname='explore-menu-list-item'>
+                    <img src={item.menu_image} alt="" />
+                 </div>
+                )
+            })}
+
+        </div>
+        </div>
+  )
+}
+
+export default ExploreMenu
